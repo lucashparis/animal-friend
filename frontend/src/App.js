a33b65572fca71237bf8939dfe3641d12ca389be
Lucas Paris <lucashenriqueparis@gmail.com>
2022-12-18 22:56:11 -0300

    Add template to test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+    </Router>
   );
 }
 
