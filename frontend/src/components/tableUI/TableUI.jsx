551003d8deda6cba67346820e54b443ca403be1a
Lucas Paris <lucas.paris@ciapetro.com.br>
2022-12-23 17:48:08 -0300

    feat: Implement tableUI component pt2

--- frontend/src/components/tableUI/TableUI.jsx
+++ frontend/src/components/tableUI/TableUI.jsx
@@ -1,112 +1,230 @@
-import React, { useState } from 'react';
-import Paper from '@mui/material/Paper';
-import {
-  DataTypeProvider,
-  TreeDataState, SortingState, SelectionState, FilteringState, PagingState,
-  CustomTreeData, IntegratedFiltering, IntegratedPaging, IntegratedSorting, IntegratedSelection,
-} from '@devexpress/dx-react-grid';
-import {
-  Grid,
-  Table, TableHeaderRow, TableFilterRow, TableTreeColumn,
-  PagingPanel, TableColumnResizing, Toolbar, TableColumnVisibility, ColumnChooser,
-} from '@devexpress/dx-react-grid-material-ui';
+import React, { useCallback, useMemo, useState } from 'react';
+import MaterialReactTable from 'material-react-table';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, MenuItem, Stack, TextField, Tooltip } from '@mui/material';
+import { Delete, Edit } from '@mui/icons-material';
+import { data } from './makeData';
 
-import { tasks, employees, priorities } from './tree-data';
+const TableUI = () => {
+  const [createModalOpen, setCreateModalOpen] = useState(false);
+  const [tableData, setTableData] = useState(() => data);
+  const [validationErrors, setValidationErrors] = useState({});
 
-const EmployeeFormatter = ({ row }) => (
-  <div
-    style={{
-      display: 'flex',
-    }}
-  >
-    <div
-      style={{
-        display: 'inline-block',
-        background: 'white',
-        borderRadius: '3px',
-        width: '30px',
-        height: '30px',
-        margin: '-8px 8px -8px 0',
-        textAlign: 'center',
-      }}
-    >
-      <img
-        src={`https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/${employees.find(e => e.ID === row.Assigned_Employee_ID).Picture}`}
-        style={{
-          height: '28px',
-          margin: '0 auto',
-        }}
-        alt="Avatar"
-      />
-    </div>
-    {employees.find(e => e.ID === row.Assigned_Employee_ID).Name}
-  </div>
-);
+  const handleCreateNewRow = (values) => {
+    tableData.push(values);
+    setTableData([...tableData]);
+  };
 
-const getChildRows = (row, rows) => {
-  const childRows = rows.filter(r => r.Parent_ID === (row ? row.ID : 0));
-  return childRows.length ? childRows : null;
-};
+  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
+    if (!Object.keys(validationErrors).length) {
+      tableData[row.index] = values;
+      //send/receive api updates here, then refetch or update local table data for re-render
+      setTableData([...tableData]);
+      exitEditingMode(); //required to exit editing mode and close modal
+    }
+  };
 
-function TableUI(params) {
-  const [columns] = useState([
-    { name: 'Assigned_Employee_ID', title: 'Assigned', getCellValue: row => employees.find(e => e.ID === row.Assigned_Employee_ID).Name },
-    { name: 'Status', title: 'Status' },
-    { name: 'Priority', title: 'Priority', getCellValue: row => priorities.find(p => p.ID === row.Priority).Value },
-    { name: 'Completion', title: '% Completed', getCellValue: row => `${row.Completion}%` },
-    { name: 'Start_Date', title: 'Start Date', getCellValue: row => row.Start_Date.split('T')[0] },
-    { name: 'Due_Date', title: 'Due Date', getCellValue: row => row.Due_Date.split('T')[0] },
-  ]);
-  const [rows] = useState(tasks);
-  const [pageSizes] = useState([5, 10, 20]);
-  // 177.33
-  const [defaultColumnWidths] = useState([
-    { columnName: 'Assigned_Employee_ID', width: 177.33},
-    { columnName: 'Status', width: 177 },
-    { columnName: 'Priority', width: 177 },
-    { columnName: 'Completion', width: 177 },
-    { columnName: 'Start_Date', width: 177 },
-    { columnName: 'Due_Date', width: 177 },
-  ]);
-  const [defaultHiddenColumnNames] = useState(['Priority', 'Completion']);
-  const [tableColumnExtensions] = useState([
-    { columnName: 'Completion', align: 'right' },
-  ]);
-  const [employeeColumns] = useState(['Assigned_Employee_ID']);
+  const handleCancelRowEdits = () => {
+    setValidationErrors({});
+  };
+
+  const handleDeleteRow = useCallback((row) => {
+      if (!confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)) return;
+      //send api delete request here, then refetch or update local table data for re-render
+      tableData.splice(row.index, 1);
+      setTableData([...tableData]);
+    }, [tableData],
+  );
+
+  const getCommonEditTextFieldProps = useCallback(
+    (cell) => {
+      return {
+        error: !!validationErrors[cell.id],
+        helperText: validationErrors[cell.id],
+        onBlur: (event) => {
+          const isValid = cell.column.id === 'email' ? validateEmail(event.target.value) : validateRequired(event.target.value);
+          if (!isValid) {
+            //set validation error for cell if invalid
+            setValidationErrors({
+              ...validationErrors,
+              [cell.id]: `${cell.column.columnDef.header} is required`,
+            });
+          } else {
+            //remove validation error for cell if valid
+            delete validationErrors[cell.id];
+            setValidationErrors({
+              ...validationErrors,
+            });
+          }
+        },
+      };
+    },
+    [validationErrors],
+  );
+
+  const columns = useMemo(() => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+        enableColumnOrdering: false,
+        enableEditing: false, //disable editing on this column
+        enableSorting: false,
+        size: 80,
+      },
+      {
+        accessorKey: 'name',
+        header: 'Nome',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+        }),
+      },
+      {
+        accessorKey: 'user',
+        header: 'Usuário',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+        }),
+      },
+      {
+        accessorKey: 'email',
+        header: 'Email',
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+          type: 'email',
+        }),
+      },
+      {
+        accessorKey: 'dtRegister',
+        header: 'Cadastrado',
+        size: 80,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+          type: 'date',
+        }),
+      },
+      {
+        accessorKey: 'dtUpdate',
+        header: 'Alterado',
+        size: 80,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+          type: 'date',
+        }),
+      },
+    ],
+    [getCommonEditTextFieldProps],
+  );
 
   return (
-    <Paper>
-      <Grid rows={rows} columns={columns}>
-        <DataTypeProvider
-          for={employeeColumns}
-          formatterComponent={EmployeeFormatter}
-        />
-
-        <TreeDataState />
-        <FilteringState />
-        <SortingState />
-        <SelectionState />
-        <PagingState defaultCurrentPage={0} defaultPageSize={pageSizes[1]} />
-
-        <CustomTreeData getChildRows={getChildRows} />
-        <IntegratedFiltering />
-        <IntegratedSelection />
-        <IntegratedSorting />
-        <IntegratedPaging />
-
-        <Table columnExtensions={tableColumnExtensions} />
-        <TableColumnVisibility defaultHiddenColumnNames={defaultHiddenColumnNames} />
-        <TableColumnResizing defaultColumnWidths={defaultColumnWidths} />
-        <TableHeaderRow showSortingControls />
-        <TableFilterRow />
-
-        <Toolbar />
-        <ColumnChooser />
-
-        <PagingPanel pageSizes={pageSizes} />
-      </Grid>
-    </Paper>
+    <>
+      <MaterialReactTable
+        displayColumnDefOptions={{
+          'mrt-row-actions': {
+            muiTableHeadCellProps: {
+              align: 'center',
+            },
+            size: 120,
+          },
+        }}
+        columns={columns}
+        data={tableData}
+        editingMode="modal" //default
+        enableColumnOrdering
+        enableEditing
+        onEditingRowSave={handleSaveRowEdits}
+        onEditingRowCancel={handleCancelRowEdits}
+        renderRowActions={({ row, table }) => (
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Tooltip arrow placement="left" title="Edit">
+              <IconButton onClick={() => table.setEditingRow(row)}>
+                <Edit />
+              </IconButton>
+            </Tooltip>
+            <Tooltip arrow placement="right" title="Delete">
+              <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                <Delete />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        )}
+        renderTopToolbarCustomActions={() => (
+          <Button
+            color="secondary"
+            onClick={() => setCreateModalOpen(true)}
+            variant="contained"
+          >
+            Cadastrar
+          </Button>
+        )}
+      />
+      <CreateNewAccountModal
+        columns={columns}
+        open={createModalOpen}
+        onClose={() => setCreateModalOpen(false)}
+        onSubmit={handleCreateNewRow}
+      />
+    </>
   );
 };
+
+//example of creating a mui dialog modal for creating new rows
+export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
+  const [values, setValues] = useState(() =>
+    columns.reduce((acc, column) => {
+      acc[column.accessorKey ?? ''] = '';
+      return acc;
+    }, {}),
+  );
+
+  const handleSubmit = () => {
+    //put your validation logic here
+    onSubmit(values);
+    onClose();
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle textAlign="center">Create New Account</DialogTitle>
+      <DialogContent>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Stack
+            sx={{
+              width: '100%',
+              minWidth: { xs: '300px', sm: '360px', md: '400px' },
+              gap: '1.5rem',
+            }}
+          >
+            {columns.map((column) => (
+              <TextField
+                key={column.accessorKey}
+                label={column.header}
+                name={column.accessorKey}
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            ))}
+          </Stack>
+        </form>
+      </DialogContent>
+      <DialogActions sx={{ p: '1.25rem' }}>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button color="secondary" onClick={handleSubmit} variant="contained">
+          Create New Account
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+const validateRequired = (value) => !!value.length;
+const validateEmail = (email) =>
+  !!email.length &&
+  email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
 
 export default TableUI;
---
+++ frontend/src/components/tableUI/makeData.js
@@ -0,0 +1,26 @@
+export const myUsers = [
+  { id: 1, name: "Lucas", user: "OldDead", status: "Ativo", email: "lucashenriqueparis@gmail.com", dtRegister: "2022-12-23", dtUpdate: "2022-12-23" },
+  { id: 2, name: "Regis", user: "Kisper", status: "Ativo", email: "regis@gmail.com", dtRegister: "2022-12-10", dtUpdate: "2022-12-23"},
+  { id: 3, name: "Dan", user: "DanPalmeirense", status: "Inativo", email: "danilo@gmail.com", dtRegister: "2022-12-05", dtUpdate: "2022-12-23"},
+  { id: 4, name: "Rafa", user: "Rafinha", status: "Inativo", email: "rafa@gmail.com", dtRegister: "2022-12-23", dtUpdate: "2022-12-23"}
+];
+
+export const data = [
+  {
+    id: '9s41rp',
+    name: 'Kelvin',
+    user: 'Langosh',
+    email: 'Jerod14@hotmail.com',
+    dtRegister: '25/02/2022',
+    dtUpdate: '23/12/2022',
+  },
+  {
+    id: '9s41sp',
+    name: 'Lucas Paris',
+    user: 'OldDead',
+    email: 'lucashenriqueparis@gmail.com',
+    dtRegister: '25/02/2022',
+    dtUpdate: '23/12/2022',
+  }
+];
+
