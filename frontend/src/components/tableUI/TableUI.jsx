636588004842ef43962a114b813279b304e06b0c
Lucas Paris <lucas.paris@ciapetro.com.br>
2022-12-26 13:07:39 -0300

    feat: Implement tableUI component pt4 (separating modal)

--- frontend/src/components/tableUI/TableUI.jsx
+++ frontend/src/components/tableUI/TableUI.jsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import MaterialReactTable from 'material-react-table';
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, MenuItem, Stack, TextField, Tooltip } from '@mui/material';
+import { Box, Button, IconButton, Tooltip } from '@mui/material';
 import { Delete, Edit } from '@mui/icons-material';
 import { data } from './makeData';
+import { CreateNewUserModal } from '../createNewUserModal/CreateNewUserModal';
 
 const TableUI = () => {
   const [createModalOpen, setCreateModalOpen] = useState(false);
@@ -160,62 +161,13 @@ const TableUI = () => {
           </Button>
         )}
       />
-      <CreateNewAccountModal
+      <CreateNewUserModal
         columns={columns}
         open={createModalOpen}
         onClose={() => setCreateModalOpen(false)}
         onSubmit={handleCreateNewRow}
       />
     </>
-  );
-};
-
-//example of creating a mui dialog modal for creating new rows
-export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
-  const [values, setValues] = useState(() => columns.reduce((acc, column) => {
-      acc[column.accessorKey ?? ''] = '';
-      return acc;
-    }, {}),
-  );
-
-  const handleSubmit = () => {
-    //put your validation logic here
-    onSubmit(values);
-    onClose();
-  };
-
-  return (
-    <Dialog open={open}>
-      <DialogTitle textAlign="center">Criar novo usuário</DialogTitle>
-      <DialogContent>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <Stack
-            sx={{
-              width: '100%',
-              minWidth: { xs: '300px', sm: '360px', md: '400px' },
-              gap: '1.5rem',
-            }}
-          >
-            {columns.map((column) => (
-              <TextField
-                key={column.accessorKey}
-                label={column.header}
-                name={column.accessorKey}
-                onChange={(e) =>
-                  setValues({ ...values, [e.target.name]: e.target.value })
-                }
-              />
-            ))}
-          </Stack>
-        </form>
-      </DialogContent>
-      <DialogActions sx={{ p: '1.25rem' }}>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button color="secondary" onClick={handleSubmit} variant="contained">
-          Create New Account
-        </Button>
-      </DialogActions>
-    </Dialog>
   );
 };
 
