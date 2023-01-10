e14e5283d08253908bafdbd49e339b8c77197010
Lucas Paris <lucashenriqueparis@gmail.com>
2023-01-09 21:10:36 -0300

    feat: Implement new component MyAlert

--- frontend/src/components/myAlert/MyAlert.jsx
+++ frontend/src/components/myAlert/MyAlert.jsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
 
 function MyAlert(props) {
-    const typeOfAlert = props.title.toLowerCase();
+  const [open, setOpen] = useState(true);
 
-    return (
-        <Stack className="flex flex-row justify-end" spacing={2}>
-            <Alert className="w-1/3 absolute" severity={typeOfAlert}>
-                <AlertTitle><strong> { props.title ? props.title : 'Error' }</strong></AlertTitle>
-                {props.message}
-            </Alert>
-        </Stack>
-    );
+  return (
+    <Box sx={{ width: '100%'}}>
+      <Collapse in={open}>
+        <Alert color={props.color}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {props.message}
+        </Alert>
+      </Collapse>
+    </Box>
+  );
 }
 
 export { MyAlert };
