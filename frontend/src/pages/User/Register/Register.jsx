c879d278cadd8ec205dfc95cd62b2f7ffc688097
Lucas Paris <lucashenriqueparis@gmail.com>
2023-01-09 21:13:02 -0300

    feat: Starts implementing custom validations

--- frontend/src/pages/User/Register/Register.jsx
+++ frontend/src/pages/User/Register/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { FormControl, TextField, InputLabel, InputAdornment, IconButton, Input, Autocomplete, Button } from "@mui/material";
+import { FormControl, TextField, InputLabel, InputAdornment, IconButton, Input, Autocomplete, Button, FormHelperText } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 import Sidebar from "../../../components/sidebar/Sidebar";
@@ -20,6 +20,13 @@ function Register() {
     const [status, setStatus] = useState(null);
     const [error, setError] = useState({});
 
+    const [fieldNameWithError, setFieldNameWithError] = useState({ message: '' });
+    const [fieldUserWithError, setFieldUserWithError] = useState({ message: '' });
+    const [fieldEmailWithError, setFieldEmailWithError] = useState({ message: '' });
+    const [fieldPasswordWithError, setFieldPasswordWithError] = useState({ message: '' });
+    const [fieldConfirmPasswordWithError, setFieldConfirmPasswordWithError] = useState({ message: '' });
+    const [fieldStatusdWithError, setFieldStatusdWithError] = useState({ message: '' });
+
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -33,41 +40,118 @@ function Register() {
         event.preventDefault();
     };
 
-    const createNewUser = async () => {
+    
+    const validFieldName = () => {
         if (!name) {
             setError({ message: 'Preencha os campos obrigatórios' });
+            setFieldNameWithError({ 
+                message: 'Esse campo não pode ser vazio!'
+            });
         }
+    }
+
+    const validFieldUser = () => {
+        const minimalCharacters = 3; 
+
+        if (!user) {
+            setError({ message: 'Preencha os campos obrigatórios' });
+            setFieldUserWithError({
+                message: 'Esse campo não pode ser vazio!'
+            });
+        } else if(user.length < minimalCharacters){
+            setError({ message: 'Preencha os campos obrigatórios' });
+            setFieldUserWithError({
+                message: `Minimo de ${minimalCharacters} caracteres`
+            });
+        }
+    }
+    
+    const validFieldEmail = () => {
+        if (!email) {
+            setError({ message: 'Preencha os campos obrigatórios' });
+            setFieldEmailWithError({
+                message: 'Esse campo não pode ser vazio!'
+            });
+        }
+    }
+
+    const validFieldPassword = () => {
+        if (!password) {
+            setError({ message: 'Preencha os campos obrigatórios' });
+            setFieldPasswordWithError({
+                message: 'Esse campo não pode ser vazio!'
+            });
+        }
+    }
+
+    const validFieldConfirmPassword = () => {
+        if (!confirmPassword) {
+            setError({ message: 'Preencha os campos obrigatórios' });
+            setFieldConfirmPasswordWithError({
+                message: 'Esse campo não pode ser vazio!'
+            });
+        }
+    }
+    
+    const validFieldStatus = () => {
+        if (!status) {
+            setError({ message: 'Preencha os campos obrigatórios' });
+            setFieldStatusdWithError({
+                message: 'Esse campo não pode ser vazio!'
+            });
+        }
+    }
+
+    const createNewUser = async () => {
+
+        const minimumCharactersForUser = (user.length > 3) ? true : false;
+
+        validFieldName();
+        validFieldUser();
+        validFieldEmail();
+        validFieldPassword();
+        validFieldConfirmPassword();
+        validFieldStatus();
+        
+
+        if (minimumCharactersForUser) {
+            setError({ message: 'Minimo de 4 caracteres para o campo usuário' });
+        }
+
         const objUser = {
             name: name,
             user: user,
-            password: password,
             email: email,
+            password: password,
             status: status,
         }
-        
+
         await createNewUserAction(objUser);
     }
 
     return (
-        <div class="antialiased bg-black w-full min-h-screen text-slate-300 relative py-4">
+        <div class="antialiased bg-my-gray w-full min-h-screen text-slate-300 relative py-4">
             <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
 
                 <Sidebar />
 
-                <div id="content" class="bg-white/10 col-span-9 rounded-lg p-6">
+                <div id="content" class="bg-my-black col-span-9 rounded-lg p-6">
 
                     { error && error.message && (
-                        <MyAlert title={'Error'} message={error.message} />
+                        <MyAlert color={'error'} message={error.message} />
                     )}
 
 
                     <div class='flex justify-between'>
-                        <TextField id="name" label="Nome" variant="standard" onChange={(e) => setName(e.target.value)} />
-                        <TextField id="user" label="Usuário" variant="standard" onChange={(e) => setUser(e.target.value)} />
-                        <TextField id="email" label="Email" variant="standard" onChange={(e) => setEmail(e.target.value)} />
+                        <TextField id="name" label="Nome" variant="standard" required onChange={(e) => setName(e.target.value)} 
+                            error={fieldNameWithError.message.length > 0} helperText={fieldNameWithError.message ? fieldNameWithError.message : ''} />
+                        <TextField id="user" label="Usuário" variant="standard" required onChange={(e) => setUser(e.target.value)} 
+                            error={fieldUserWithError.message.length > 0} helperText={fieldUserWithError.message ? fieldUserWithError.message : ''} />
+                        <TextField id="email" label="Email" variant="standard" required onChange={(e) => setEmail(e.target.value)} 
+                            error={fieldEmailWithError.message.length > 0} helperText={fieldEmailWithError.message ? fieldEmailWithError.message : ''} />
                     </div>
                     <div class='flex justify-between'>
-                        <FormControl variant="standard">
+                        <FormControl variant="standard" error={fieldPasswordWithError.message.length > 0}>
                             <InputLabel htmlFor="password"> Senha </InputLabel>
                             <Input
                                 id="password"
@@ -83,10 +167,13 @@ function Register() {
                                         </IconButton>
                                     </InputAdornment>
                                 }
+                                required
                                 onChange={(e) => setPassword(e.target.value)}
                             />
+                            <FormHelperText id="my-helper-text">{fieldPasswordWithError.message}</FormHelperText>
                         </FormControl>
-                        <FormControl variant="standard">
+
+                        <FormControl variant="standard" error={fieldPasswordWithError.message.length > 0}>
                             <InputLabel htmlFor="confirmPassword"> Confirmar Senha </InputLabel>
                             <Input
                                 id="confirmPassword"
@@ -102,18 +189,24 @@ function Register() {
                                         </IconButton>
                                     </InputAdornment>
                                 }
+                                required
                                 onChange={(e) => setConfirmPassword(e.target.value)}
                             />
+                            <FormHelperText id="my-helper-text">{fieldConfirmPasswordWithError.message}</FormHelperText>
                         </FormControl>
-                        <Autocomplete
-                            options={statusOptions}
-                            id="status"
-                            autoSelect
-                            renderInput={(params) => (
-                                <TextField {...params} label="Status" variant="standard" />
-                            )}
-                            onChange={(e, value) => setStatus(value)}
-                        />
+
+                        <FormControl error={fieldStatusdWithError.message.length > 0}>
+                            <Autocomplete
+                                options={statusOptions}
+                                id="status"
+                                autoSelect
+                                renderInput={(params) => (
+                                    <TextField {...params} label="Status" variant="standard" />
+                                    )}
+                                    onChange={(e, value) => setStatus(value)}
+                            />
+                            <FormHelperText id="my-helper-text">{fieldStatusdWithError.message}</FormHelperText>
+                        </FormControl>
                     </div>
                     <div className="flex justify-end">
                         <Button variant="contained" color="success"
