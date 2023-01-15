a6cc575dee619cf06e2d6b9210c334350e916afa
Lucas Paris <lucashenriqueparis@gmail.com>
2023-01-15 13:57:05 -0300

    feat: Encrypt password to save in DB

--- backend/src/services/user/createUserService.js
+++ backend/src/services/user/createUserService.js
@@ -1,29 +1,33 @@
-import database from '../../../db'
-import User from '../../models/user'
+import database from '../../../db';
+import User from '../../models/user';
+import bcrypt from "bcrypt";
 
 const execute = async (objUser) => {
-  let transaction;
+    let transaction;
 
-  try {
-    if (!objUser) return;
+    try {
+        if (!objUser) return;
 
-    transaction = await database.transaction();
-    
-    const modelUser = {
-      nome: objUser.name,
-      usuario: objUser.user,
-      email: objUser.email,
-      senha: objUser.password,
-      status: objUser.status,
-    };
-    
-    const user = await User.create(modelUser);
-    await transaction.commit();
-    return user;
-  } catch (error) {
-      if (transaction) transaction.rollback();
-      throw error;
-  }
+        transaction = await database.transaction();
+       
+        const saltRounds = 10;
+        const hash = bcrypt.hashSync(objUser.password, saltRounds);
+
+        const modelUser = {
+            nome: objUser.name,
+            usuario: objUser.user,
+            email: objUser.email,
+            senha: hash,
+            status: objUser.status,
+        };
+
+        const user = await User.create(modelUser);
+        await transaction.commit();
+        return user;
+    } catch (error) {
+        if (transaction) transaction.rollback();
+        throw error;
+    }
 }
 
 export { execute };
