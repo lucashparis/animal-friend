90087fd26b9c12ac453ff85e897c5eb2f25c28d2
Lucas Paris <lucashenriqueparis@gmail.com>
2023-01-16 21:45:35 -0300

    refactor: Remove css button component

--- frontend/src/components/button/Button.jsx
+++
@@ -1,7 +0,0 @@
-function Button(props) {
-    return (
-        <button type="button" class="inline-block h-3/4 px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"> {props.name} </button>
-    );
-}
-
-export { Button };
